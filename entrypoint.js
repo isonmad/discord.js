491a7bf4271ebef099cb8246dc8092cb9046ec99
abalabahaha <hi@abal.moe>
2016-10-17 03:06:44 +0900

    Remove more items

--- entrypoint.js
+++ entrypoint.js
@@ -1,6 +1,7 @@
 module.exports.Client = function Client() {
-    console.log("You appear to have installed discord.js from hydrabolt/discord.js#indev-old." +
-    "This has been moved to hydrabolt/discord.js#v8. Please update your references accordingly.");
+    console.log("#####################################################\n" +
+        "[ERROR] You appear to have installed discord.js from hydrabolt/discord.js#indev-old." +
+        "This has been moved to hydrabolt/discord.js#v8. Please update your references accordingly.");
 }
 
 module.exports.Client();
